efb8043dfa7069de0ba34ac1e80a24c7c61c31bf
amul patle <amulpatle3@gmail.com>
2024-09-30 12:00:00 +0530

    create header and signIn form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
+import Body from './components/Body';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Namaste everyone</h1>
-    </div>
+    <Body/>
   );
 }
 

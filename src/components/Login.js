b97a84bb59f1a6428aa63baefbc074aa1fb5c161
amul patle <amulpatle3@gmail.com>
2024-09-30 14:30:20 +0530

    create sign up form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import Header from './Header';
+import { useState } from 'react';
 
 const Login = () => {
+
+  const [isSignForm,setIsSignInForm] = useState(true)
+
+  const toggelSignInForm = ()=>{
+    setIsSignInForm(!isSignForm)
+  }
   return (
     <div>
         <Header/>
@@ -12,7 +19,12 @@ const Login = () => {
             />
         </div>
         <form className='w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white bg-opacity-80 rounded-lg'>
-        <h1 className='font-bold text-3xl py-4'>Sign In</h1>
+        <h1 className='font-bold text-3xl py-4'>{isSignForm?"Sign In":"Sign Up"}</h1>
+            {!isSignForm && <input 
+              type='text' 
+              placeholder='Full Name' 
+              className='p-4 my-4 w-full bg-gray-700' 
+            />}
             <input 
               type='text' 
               placeholder='Email Address' 
@@ -24,9 +36,12 @@ const Login = () => {
               className='p-4 my-4 w-full bg-gray-700' 
             />
             <button className='p-4 my-6 bg-red-700 w-full rounded-lg'>
-              Sign In
+              {isSignForm?"Sign In":"Sign Up"}
             </button>
-            <p className='py-4'>New to Netflix? Sign Up Now</p>
+            <p className='py-4 cursor-pointer' onClick={toggelSignInForm}>
+              {isSignForm 
+              ?"New to Netflix? Sign Up Now"
+              :"Already Registered? Sign In Now."}</p>
         </form>
     </div>
   );
